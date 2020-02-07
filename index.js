98da9b6558f0a26be2ba45114afda07b2973f3a9
Kellen Presta <kpresta04@gmail.com>
2020-02-07 17:57:16 -0600

    added axios

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
+const inquirer = require("inquirer");
+const axios = require("axios");
+
 function main() {
   // Create variables
 
   let favcolor;
   let profileImage;
   let username;
+
+  async function getColor() {
+    try {
+      favcolor = await inquirer.prompt({
+        message: "Favorite color?",
+        name: "color"
+      });
+      username = await inquirer.prompt({
+        message: "What is your git username?",
+        name: "username"
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  getColor();
 }
+
+main();
