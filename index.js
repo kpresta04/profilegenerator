c02fb098f856e495c604e7e87889f1d8bed8b516
Kellen Presta <kpresta04@gmail.com>
2020-02-07 23:26:58 -0600

    avatar added

--- index.js
+++ index.js
@@ -37,8 +37,10 @@ function main() {
         `https://api.github.com/users/${username}/repos?per_page=100`
       );
       const data = await response.data;
+      const avatar = await axios.get(`${data[0].owner.avatar_url}`);
       console.log("Number of repos: " + data.length);
       console.log(data[0].owner);
+      //   console.log(avatar);
     } catch (error) {
       console.log(error);
     }
