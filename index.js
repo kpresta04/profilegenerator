350699a46b24b31d08a9c957614c87d7f95efcb7
Kellen Presta <kpresta04@gmail.com>
2020-02-09 17:12:16 -0600

    integrating function

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 const inquirer = require("inquirer");
 const axios = require("axios");
+const fs = require("fs");
+
+const colors = {
+  green: {
+    wrapperBackground: "#E6E1C3",
+    headerBackground: "#C1C72C",
+    headerColor: "black",
+    photoBorderColor: "#black"
+  },
+  blue: {
+    wrapperBackground: "#5F64D3",
+    headerBackground: "#26175A",
+    headerColor: "white",
+    photoBorderColor: "#73448C"
+  },
+  pink: {
+    wrapperBackground: "#879CDF",
+    headerBackground: "#FF8374",
+    headerColor: "white",
+    photoBorderColor: "#FEE24C"
+  },
+  red: {
+    wrapperBackground: "#DE9967",
+    headerBackground: "#870603",
+    headerColor: "white",
+    photoBorderColor: "white"
+  }
+};
 
 function main() {
   // Create variables
